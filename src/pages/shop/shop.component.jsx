fe3f6e3ce5d18790c4e475d9249912f8a8304ca7
Maria Poslihalina <mihazed@gmail.com>
2020-02-06 13:31:53 +0200

    updated the shopping page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SHOP_DATA from "../shop/shop.data";
 import CollectionPreview from "../../components/preview-collecion/collection-preview.component";
+
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
